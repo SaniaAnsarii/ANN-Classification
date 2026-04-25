2846506c97544dd7fa69118590cc6cb465444e04
SaniaAnsarii <ansarisania789@gmail.com>
2026-04-26 04:19:35 +0530

    initial

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
 import pandas as pd
 import pickle
 
-# Load the trained model
+
 model = tf.keras.models.load_model('model.h5')
 
-# Load the encoders and scaler
+
 with open('label_encoder_gender.pkl', 'rb') as file:
     label_encoder_gender = pickle.load(file)
 
@@ -19,10 +19,10 @@ with open('scaler.pkl', 'rb') as file:
     scaler = pickle.load(file)
 
 
-## streamlit app
+
 st.title('Customer Churn PRediction')
 
-# User input
+
 geography = st.selectbox('Geography', onehot_encoder_geo.categories_[0])
 gender = st.selectbox('Gender', label_encoder_gender.classes_)
 age = st.slider('Age', 18, 92)
@@ -34,7 +34,7 @@ num_of_products = st.slider('Number of Products', 1, 4)
 has_cr_card = st.selectbox('Has Credit Card', [0, 1])
 is_active_member = st.selectbox('Is Active Member', [0, 1])
 
-# Prepare the input data
+
 input_data = pd.DataFrame({
     'CreditScore': [credit_score],
     'Gender': [label_encoder_gender.transform([gender])[0]],
